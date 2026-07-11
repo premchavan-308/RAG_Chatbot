12abc0e71b7143d145e5b179dc6b5e684c24508c
premchavan-308 <premchavan328@gmail.com>
2026-07-11 11:31:43 +0530

    Refactor create_vector_db function
    Removed unnecessary cleanup and persistence directory handling for vector database creation.

--- ingest.py
+++ ingest.py
@@ -6,17 +6,10 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_mistralai import MistralAIEmbeddings
 from langchain_chroma import Chroma
-import os
-import shutil
-import gc
+
 
 def create_vector_db(pdf_path):
 
-    gc.collect()
-
-    if os.path.exists("chroma_db"):
-        shutil.rmtree("chroma_db", ignore_errors=True)
-    
     loader = PyPDFLoader(pdf_path)
     docs = loader.load()
 
@@ -32,10 +25,11 @@ def create_vector_db(pdf_path):
     )
 
     db = Chroma.from_documents(
-    documents=chunks,
-    embedding=embedding,
-    persist_directory="chroma_db",
-    collection_name="rag_pdf"
+        documents=chunks,
+        embedding=embedding,
+        collection_name="rag_pdf"
     )
 
     print("✅ Vector Database Created")
+
+    return db
